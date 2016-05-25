49a8a6e773d7a634f56f2d821852287ba4610253
mitra00 <mitra.shubhodeep@gmail.com>
2016-05-25 18:03:25 +0530

    Initial Commit

--- app/src/main/java/com/mitrastudios/www/restaurantbill/MainActivity.java
+++ app/src/main/java/com/mitrastudios/www/restaurantbill/MainActivity.java
@@ -2,8 +2,12 @@ package com.mitrastudios.www.restaurantbill;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +15,34 @@ public class MainActivity extends AppCompatActivity {
     //for the plus sign button
     private Button mPlus;
 
-    //for minus sign button
-    private Button mMinus;
-
-    //for equalto sign button
+    //for minus equal to button
     private Button mEqual;
+
+    //for Clr sign button
+    private Button mClr;
 
     //for Bill
     private Button mBill;
+
+    //for edit text
+    private EditText mEditText;
+
+    /**
+     * a variable that will track that whether the equal to sign is clicked
+     *
+     * if the equal is 1 and next time plus sign is clicked the edit text value won't be added
+     */
+    private int eq;
+
+    //for Vat
+
+
+    // to store the number
+    private int mTemp;
+    //to store the sum of the items
+    private int mSum;
+    //to store the final bill
+    private double mTotal;
 
 
     @Override
@@ -27,9 +51,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPlus=(Button)findViewById(R.id.buttonPlus);
-        mMinus=(Button)findViewById(R.id.buttonMinus);
         mEqual=(Button)findViewById(R.id.buttonEqual);
+        mClr=(Button)findViewById(R.id.buttonClr);
         mBill=(Button)findViewById(R.id.buttonBill);
+        mEditText=(EditText)findViewById(R.id.editText);
+
+        //initializing the value of the sum to be zero
+        mSum=0;
+
+        //initializing the value of the eq with zero
+        eq=0;
+
+
+
+
+
 
 
         /**
@@ -39,16 +75,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-            }
-        });
+                /**
+                 * a variable that will track that whether the equal to sign is clicked
+                 *
+                 * if the equal is 1 and next time plus sign is clicked the edit text value won't be added
+                 *
+                 *
+                 *
+                 * previously what was happening was that after clicking the equal to sign we were getting the result,
+                 * but immediately after clicking plus sign would add the result got as a new parameter.
+                 */
 
+                if(eq==0) {
+                    //getting the value from edit text and converting it to int
+                    mTemp = Integer.parseInt(mEditText.getText().toString());
+                    mSum += mTemp;
+                }
+                /**
+                 * removing the numbers||| resetting the edit text
+                 */
+                mEditText.getText().clear();
 
-        /**
-         * operations to be done when minus sign is clicked
-         */
-        mMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                //resetting the equal to sign clicked to zero
+                eq=0;
 
             }
         });
@@ -61,6 +110,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                //getting the value from edit text and converting it to int
+                mTemp= Integer.parseInt(mEditText.getText().toString());
+                mSum+=mTemp;
+
+
+                //since equal to sign is clicked
+                eq=1;
+                //printing the sum to the editText field
+                mEditText.setText(""+mSum);
+            }
+        });
+
+
+        /**
+         * operations to be done when Clr to sign is clicked
+         */
+        mClr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //resetting the edit field
+                mEditText.getText().clear();
+                //resetting th value of mSum
+                mSum=0;
             }
         });
 
@@ -74,6 +146,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+
 
     }
 }
